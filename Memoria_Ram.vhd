aaac9cab7e8e0e1a56b3e358fc7e1f6b14c34ec1
Hilary Madeley Calva Camacho <105516501+Hilary-015@users.noreply.github.com>
2022-06-19 14:31:33 -0500

    Add files via upload

---
+++ Memoria_Ram.vhd
@@ -0,0 +1,60 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    22:37:06 06/18/2022 
+-- Design Name: 
+-- Module Name:    Memoria_Ram - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Memoria_Ram is
+	Port(
+		clk: in STD_LOGIC;
+		entrada, direccion: in STD_LOGIC_VECTOR(1 downto 0);
+		CE, OE, WE: in STD_LOGIC;
+		salida: out STD_LOGIC_VECTOR(1 downto 0)
+	);
+end Memoria_Ram;
+
+architecture Behavioral of Memoria_Ram is
+
+type datos is array (0 to 3) of STD_LOGIC_VECTOR(1 downto 0);
+signal signalDatos : datos;
+begin
+process(clk)
+begin
+	if CE = '0' then
+		if (clk'event and clk='1') then
+			if WE = '0' then
+				signalDatos(to_integer(unsigned(direccion))) <= entrada;
+			else
+				salida <= signalDatos(to_integer(unsigned(direccion)));
+			end if;
+		end if;
+	end if;
+end process;
+
+end Behavioral;
---
+++ TB_Memoria_Ram.vhd
@@ -0,0 +1,98 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    22:40:08 06/18/2022 
+-- Design Name: 
+-- Module Name:    TB_Memoria_Ram - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity TB_Memoria_Ram is
+end TB_Memoria_Ram;
+
+architecture Behavioral of TB_Memoria_Ram is
+
+    component Memoria_Ram
+    PORT(
+         clk : IN  std_logic;
+         entrada : IN  std_logic_vector(1 downto 0);
+         direccion : IN  std_logic_vector(1 downto 0);
+         CE : IN  std_logic;
+         OE : IN  std_logic;
+         WE : IN  std_logic;
+         salida : OUT  std_logic_vector(1 downto 0)
+        );
+    end component;
+    
+
+   --Entradas
+   signal clk : std_logic := '0';
+   signal entrada : std_logic_vector(1 downto 0) := (others => '0');
+   signal direccion : std_logic_vector(1 downto 0) := (others => '0');
+   signal CE : std_logic := '0';
+   signal OE : std_logic := '0';
+   signal WE : std_logic := '0';
+
+ 	--Salidas
+   signal salida : std_logic_vector(1 downto 0);
+
+   -- 
+   constant clk_period : time := 10 ns;
+ 
+begin
+
+   UUT: Memoria_Ram port map (
+          clk => clk,
+          entrada => entrada,
+          direccion => direccion,
+          CE => CE,
+          OE => OE,
+          WE => WE,
+          salida => salida
+        );
+
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+
+   stim_proc: process
+   begin		
+      CE <= '0';
+		WE <= '0';
+		OE <= '1';
+		entrada <= "11";
+		direccion <= "00";
+      wait for clk_period;
+		WE <= '1';
+		OE <= '0';
+		direccion <= "00";		
+      wait;
+   end process;
+end Behavioral;
+
